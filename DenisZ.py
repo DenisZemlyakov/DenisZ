bcb1f41c1402f040fcde8934db4838f050201eb1
opilane <opilane@HP-CZC6297B8Z>
2021-10-29 11:26:32 +0300

    ttttttt

--- DenisZ.py
+++ DenisZ.py
@@ -1,4 +1,53 @@
 import random
+v1=choice(["kivi","käärid","paber"])
+v2=choice(["kivi","käärid","paber"])
+if m==1:
+    while True:
+        print("kas mängime? esc - valja, enter - mängime")
+        if read_key()="esc":
+            break
+        elif read_key()=="enter" :
+         p1=choice(v1)
+         print("esimine bot: ",p1)
+         p2=choice(v2)
+         print("teine bot: ",p2)
+         if p1==p2:
+             print("viik")
+        elif p1==v1[0] and p2==v2[1] or p1==v1[2] and p2==v2[0] or p1==v[1 and p2==v2[2]:
+         print("võitis 1")
+        else:
+        print("võtis2")     
+comp = random.randint(1, 3)
+if comp == 1:
+        print("kivi.") 
+if comp == 2:
+        print("käärid.")
+if comp == 3:
+        print("paber.")
+if p1 == p2:
+        win = 0
+if p1 == 1 and p2 == 2:
+        win = 1 
+if p1 == 1 and p2 == 3:
+        win = 2 
+if p1 == 2 and p2 == 1:
+        win = 2  
+if p1 == 2 and p2 == 3:
+        win = 1 
+if p1 == 3 and p2 == 1:
+        win = 1
+if p1 == 3 and p2 == 2:
+        win = 2
+if win == 0:
+        print("Ничья!")
+if win == 1:
+        print("ты Победил")
+if win == 2:
+        print("Победил компьютер!")
+
+print()
+
+import random
 ver = 0
 while (ver == 0):
         player = int(input("1 - камень, 2 - ножницы, 3 - бумага. "))
