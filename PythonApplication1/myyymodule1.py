918ad9adda2493e4226e6e4bdd229bd1896d06a8
Denis Zemlyakov <denisino@bk.ru>
2021-11-10 19:28:37 +0200

    дз

--- PythonApplication1/myyymodule1.py
+++ PythonApplication1/myyymodule1.py
@@ -34,5 +34,19 @@ def is_year_leap(aasta: int):
 				else:
 								vastus=False
 				return vastus
-def square(sude:float):
-				"""square
+
+"""
+Квадрат
+=======
+Написать функцию square, принимающую 1 аргумент — сторону квадрата, и возвращающую 3 значения (с помощью кортежа):
+периметр квадрата, площадь квадрата и диагональ квадрата.
+"""
+import math
+
+def square(a):
+
+    p = 4 * a
+    s = a ** 2
+    diag = a * math.sqrt(2)
+
+    return p, s, diag
